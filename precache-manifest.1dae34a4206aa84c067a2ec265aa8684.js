aa0a7474f533fa3b2527d883d996a5e55cc0f2b5
Talal Munzar <tmunzar@gmail.com>
2020-04-17 14:12:47 -0400

    deploy new

---
+++ precache-manifest.1dae34a4206aa84c067a2ec265aa8684.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a8b164883ab40abf767c423a81cf55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "829f080e0ac680cc27dc",
+    "url": "/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "214a9f5ce0e5123e66d1",
+    "url": "/static/css/main.8a555304.chunk.css"
+  },
+  {
+    "revision": "829f080e0ac680cc27dc",
+    "url": "/static/js/2.5bca0090.chunk.js"
+  },
+  {
+    "revision": "21323f5f119d9a054e0e875239d553a6",
+    "url": "/static/js/2.5bca0090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214a9f5ce0e5123e66d1",
+    "url": "/static/js/main.7d0963fc.chunk.js"
+  },
+  {
+    "revision": "ae4ec2866aff62a2a48d",
+    "url": "/static/js/runtime-main.e0804590.js"
+  },
+  {
+    "revision": "aaf072bdef2576ba8412b2dabb6d1681",
+    "url": "/static/media/me-with-cup-01.aaf072bd.png"
+  }
+]);
